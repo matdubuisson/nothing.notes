a2fd01bd706c689d3e2bfe98826c3b45a2b5a519
Luis Schröder <kresstudios@gmail.com>
2024-12-06 19:00:38 +0100

    Notifications📟  are finally here

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,11 @@ import 'package:nothing_note/irno/firebase_options.dart';
 import 'package:nothing_note/services/auth.dart';
 import 'package:nothing_note/services/firebase_api.dart';
 import 'package:nothing_note/services/task_provider.dart';
+import 'package:nothing_note/tasks_page.dart';
 import 'package:nothing_note/theme.dart';
 import 'package:provider/provider.dart';
+
+final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -30,6 +33,10 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         home: AuthPage(),
         theme: Provider.of<ThemeProvider>(context).themeData,
+        navigatorKey: navigatorKey,
+        routes: {
+      '/tasks_page': (context) => const TasksPage(),
+    },
       ),
     );
   }
--- lib/services/firebase_api.dart
+++ lib/services/firebase_api.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:nothing_note/main.dart';
 
 class FirebaseApi {
   // create an instance of Firebase Messaging
@@ -18,6 +19,21 @@ class FirebaseApi {
   }
 
   // function to handle received messages
+  void handleMessage(RemoteMessage? message) {
+    if (message == null) return;
+
+    // navigate to screen
+    navigatorKey.currentState?.pushNamed(
+      '/tasks_page',
+      arguments: message,
+    );
+  }
 
   // function to initialize foreground and background settings
+  Future initPushNotifications() async {
+    // handle notification if the app was terminated and now opened
+    FirebaseMessaging.instance.getInitialMessage().then(handleMessage);
+
+    FirebaseMessaging.onMessageOpenedApp.listen(handleMessage);
+  }
 }
